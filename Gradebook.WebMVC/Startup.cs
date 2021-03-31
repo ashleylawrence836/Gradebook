d0faa32849b099fad49145c9eed6ee9577021538
Ashley Lawrence <ashleylawrence836@mail.com>
2021-03-31 13:42:09 -0400

    Add project files.

---
+++ Gradebook.WebMVC/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(Gradebook.WebMVC.Startup))]
+namespace Gradebook.WebMVC
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
